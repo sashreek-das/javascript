82849e5172f7d44994e8c5442ef9fa5e9e5c7bdd
pratik_codes <66104012+pratik123-coder@users.noreply.github.com>
2024-01-27 13:55:24 +0530

    js_commit

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello World")
+const accessKey="yFYl4FcEt7KFmKMrTdFwpg0VSp64BSN_W0ppTla0O_U"
+
+const formEl=document.querySelector("form")
+const inputEl=document.getElementById("search-input")
+const searchResults=document.querySelector(".search-results")
+const showMore=document.getElementById("show-more")
+
+let inputData=""
+let page=1;
+ async function searchImage(){
+    inputData=inputEl.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`
+ }
